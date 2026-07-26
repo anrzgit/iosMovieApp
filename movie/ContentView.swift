957fde8a367c39b163ccaf5cdb4aa50f5780aa0c
aditya-detect <aditya.k@detecttechnologies.com>
2026-07-26 23:26:36 +0530

    Initial Commit

---
+++ movie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie
+//
+//  Created by aditya on 26/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie/movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieApp.swift
+//  movie
+//
+//  Created by aditya on 26/07/26.
+//
+
+import SwiftUI
+
+@main
+struct movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
